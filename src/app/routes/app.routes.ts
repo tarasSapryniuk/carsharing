fe44be8d1a9dea95cc789654d5bae2263e1a6cef
tarasSapryniuk <44494173+tarasSapryniuk@users.noreply.github.com>
2019-12-24 21:46:40 +0200

    Customized routing and added new car components

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from "@angular/router";
-import { CarsComponent } from './cars/cars.component';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 
 export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'settings', component: SettingsComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+    { path: 'cars', component: CarDetailComponent },
     { path: '**', component: HomeComponent }
 ]
